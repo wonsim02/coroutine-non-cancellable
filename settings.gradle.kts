71d024710990cc23272b32109785c762a6774324
wonsim02 <wonsim0718@gmail.com>
2021-12-29 02:38:59 +0900

    init project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "coroutine-non-cancellable"
---
+++ src/test/kotlin/com/example/coroutinenoncancellable/AppTest.kt
@@ -0,0 +1,10 @@
+package com.example.coroutinenoncancellable
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppTest {
+	@Test
+	fun contextLoads() { }
+}
